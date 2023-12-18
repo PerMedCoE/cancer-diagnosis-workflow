41ef267d2798db52420a07fb3a4c1ee018b5ef0a
fconejer <francisco.conejero@bsc.es>
2023-12-18 13:23:40 +0100

    Set all folders within the same partition and using python 3.10.

--- Workflows/PyCOMPSs/launch.sh
+++ Workflows/PyCOMPSs/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export COMPSS_PYTHON_VERSION=3
+export COMPSS_PYTHON_VERSION=3.10.2
 module load COMPSs/3.3
 module load singularity/3.7.3
 module use /apps/modules/modulefiles/tools/COMPSs/libraries
@@ -14,14 +14,20 @@ models=$(pwd)/../../Resources/models/
 # Set the tool internal parallelism and constraint
 export COMPUTING_UNITS=1
 
+mkdir worker_wd
+mkdir master_wd
+mkdir log_dir
+
 enqueue_compss \
     --qos=debug \
     --num_nodes=2 \
-    --exec_time=45 \
-    --worker_working_dir=$(pwd) \
-    --log_level=debug \
+    --exec_time=80 \
+    --worker_working_dir=$(pwd)/worker_wd/ \
+    --master_working_dir=$(pwd)/master_wd/ \
+    --log_dir=$(pwd)/log_dir/ \
+    --log_level=off \
     --graph \
-    --python_interpreter=python3 \
+    --tracing \
     $(pwd)/src/cancer_diagnosis.py \
       --exp ${dataset}/exp.txt \
       --metadata ${dataset}/metadata.tsv \
